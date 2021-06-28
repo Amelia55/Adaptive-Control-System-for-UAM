026740fcc6c4ec4e55c4077faa5943c9ce119f23
Amelia55 <76153287+Amelia55@users.noreply.github.com>
2021-06-28 10:27:32 +0800

    Update Shortest_path.m
    The redundant parts have been deleted.

--- Shortest_path.m
+++ Shortest_path.m
@@ -1,7 +1,4 @@
  function [NextPoint,NextLT]=Shortest_path(JT_xiuzheng,JT,startpoint,endpoint)
-% JT=xlsread('C:\Users\Lenovo\Desktop\DATA\Terminal area traffic network.xlsx','JT');%终端区交通路进场网的初始值
-% startpoint=1;
-% endpoint=8;
 JT_xiuzheng(isnan(JT_xiuzheng))=inf;
 c1=zeros(1,length(JT_xiuzheng));
 c2=zeros(1,length(JT_xiuzheng));
@@ -19,19 +16,12 @@ for i=1:length(JT_xiuzheng)
     end
 end
  DG = sparse(c2,c1,JT_xiuzheng);  
-% h = view(biograph(DG,[],'ShowWeights','on')) %画图, 这个好玩
-% Find shortest path from 起点 to 终点
 for i=1:size(JT_xiuzheng,2)
     for j=2:size(JT_xiuzheng,2)
-[dist,lujing] = graphshortestpath(DG,startpoint,endpoint); %找顶点到终点的最短路径!
+[dist,lujing] = graphshortestpath(DG,startpoint,endpoint); 
 distance(i,j)=dist;
     end
 end
-% Mark the nodes and edges of the shortest path
-% set(h.Nodes(lujing),'Color',[1 0.4 0.4]) %上色
-% edges = getedgesbynodeid(h,get(h.Nodes(lujing),'ID'));
-% set(edges,'LineColor',[1 0 0]) %上色
-% set(edges,'LineWidth',1.5) %上色
 
  NextPoint=lujing(2);
  NextLT=JT(startpoint,NextPoint);
